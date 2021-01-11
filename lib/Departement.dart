b67b90e1bebff0f595350a7a0d3e7d14956299ce
Aissata Ba <aissata.ba0903@gmail.com>
2021-01-11 14:09:49 +0000

    lister les departements

--- lib/Departement.dart
+++ lib/Departement.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
 
 class Departement extends StatefulWidget{
 
@@ -10,11 +13,44 @@ class Departement extends StatefulWidget{
 }
 
 class DepartementState extends State<Departement>{
+  List<dynamic> listeDepartement;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Listes des departements du centre"),),
+      body: Center(
+          child: this.listeDepartement==null?CircularProgressIndicator():
+          ListView.builder(
+              itemCount: (this.listeDepartement==null)?0:this.listeDepartement.length,
+              itemBuilder: (context, index){
+                return Card(
+                  color: Colors.cyanAccent,
+                  child: RaisedButton(
+                    child: Text(this.listeDepartement[index]['nomdep']),
+                  ),
+                );
+              }
+          )
+      ),
     );
   }
-
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    listerDepartement();
+  }
+  void listerDepartement() {
+    String url = "http://192.168.43.79:8080/departements";
+    http.get(url)
+        .then((resp){
+      setState(() {
+        this.listeDepartement=json.decode(resp.body)['_embedded']['departements'];
+      });
+    }).catchError((err){
+      print(err);
+    });
+  }
 }
+
